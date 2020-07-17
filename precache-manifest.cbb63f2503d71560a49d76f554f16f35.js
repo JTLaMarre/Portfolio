36d91dbb2454497945c85556680fe15e5c13038d
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-17 11:44:44 -0400

    Updates

---
+++ precache-manifest.cbb63f2503d71560a49d76f554f16f35.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af77fb9649a3b7aa07d921fa0bb99ce6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0ece770b72209b075570",
+    "url": "/Portfolio/static/css/main.c5ee0cd9.chunk.css"
+  },
+  {
+    "revision": "22b60e174de701c22243",
+    "url": "/Portfolio/static/js/2.eac93596.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.eac93596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ece770b72209b075570",
+    "url": "/Portfolio/static/js/main.6d2401b3.chunk.js"
+  },
+  {
+    "revision": "796235efddf27470bbfe",
+    "url": "/Portfolio/static/js/runtime-main.25f07560.js"
+  },
+  {
+    "revision": "49d691f41a88e30d007e9218e1af68da",
+    "url": "/Portfolio/static/media/EmployScreen1.49d691f4.PNG"
+  },
+  {
+    "revision": "fb1bd075d9a171ecc003262f64f13e2f",
+    "url": "/Portfolio/static/media/LetsGuess.fb1bd075.PNG"
+  },
+  {
+    "revision": "6957237c2ffb5e398da5c88d1b916ed8",
+    "url": "/Portfolio/static/media/Pizza.6957237c.PNG"
+  },
+  {
+    "revision": "5c6dd9745a61eeba54a22955b6e94f09",
+    "url": "/Portfolio/static/media/Screenshot.5c6dd974.PNG"
+  },
+  {
+    "revision": "db182b93f6357494ab4e127d21bf8edc",
+    "url": "/Portfolio/static/media/Work day scheduler.db182b93.PNG"
+  },
+  {
+    "revision": "e29b57ab795d639e84044325010e5449",
+    "url": "/Portfolio/static/media/mePic.e29b57ab.jpg"
+  },
+  {
+    "revision": "2cc86e6e9ea63a9689f0ea248137aa90",
+    "url": "/Portfolio/static/media/trailheads.2cc86e6e.PNG"
+  },
+  {
+    "revision": "fa66c0dd3efc720ea14b64ebd1306e08",
+    "url": "/Portfolio/static/media/weatherdash.fa66c0dd.PNG"
+  }
+]);
