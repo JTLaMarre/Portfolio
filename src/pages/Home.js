8fd97b9846458ccfcdf64febd15a2f7d385564c0
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-17 12:18:29 -0400

    more about me changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,14 +12,16 @@ const Home = () => {
           <img src={require ("../images/mePic.jpg")} alt="Me" id="me"></img>
       </div>
       <div className="column is-half">
-          <h2> my info</h2>
+          <h2 className="is-size-1"> my info</h2>
           <p>Github:<a href="https://github.com/JTLaMarre">my github profile</a></p>
           <p>Linkedin:<a href="https://www.linkedin.com/in/jacob-lamarre-6b578629/">my Linkedin</a></p>
           <p>Hi! I'm Jacob, I am a full stack web developer that brings professional problem solving and relation skills to the table from my time as a pharmacy technician! Working with different pharmacy systems has given me a mountain of
             practice with problem solving and inputting data with minimal mistakes.Looking forward to the exciting challenges programming will bring my way. I love building fun projects in React. Front end work in general is exciting and allows me to either channel some of my own creativity or bring someone elses vision to the screen from given specs.</p>
-
-            <h4 className="is-4">Technology proficiencies:</h4>
+        <div className="card">
+                    <h1 className=" title is-size-2">Technology proficiencies:</h1>
             <p>the following are technologies I am comfortably proficient with</p>
+            <div className="card-content">
+
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -31,6 +33,9 @@ const Home = () => {
                 <li>mongodb</li>
                 <li>mongoose</li>
             </ul>
+            </div>
+        
+        </div>
       </div>
   </div>
         <Footer/>
