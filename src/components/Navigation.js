be0b9377f912df58419ce3cde3abe46aafe4eae3
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-18 09:49:58 -0400

    fixed burger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,47 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import './comp.css'
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
+   const [isActive, setisActive]= useState(false)
    return (
       <div className="hero-head">
       <div className="navbar is-dark">
          <div className="container">
-            <label
-               role="button"
-               className="navbar-burger burger"
-               aria-label="menu"
-               aria-expanded="false"
-               htmlFor="nav-toggle-state"
+         <label
+              role="button"
+              className={`navbar-burger burger is-dark ${isActive ? "is-active" : ""}`}
+              aria-label="menu"
+              aria-expanded="false"
+              htmlFor="nav-toggle-state"
+              
             >
-               <span aria-hidden="true"></span>
-               <span aria-hidden="true"></span>
-               <span aria-hidden="true" ></span>
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
             </label>
-               <input type="checkbox" id="nav-toggle-state"/>
-         </div>
+          </div>
+          <input type="checkbox" id="nav-toggle-state" />
 
-         <div className="navbar-menu">
+         
 
-            <div className="navbar-menu">
+            <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
                <div className="navbar-start">
-                  <NavLink className="navbar-item is-active" to="/">Home</NavLink>
-                  <NavLink className="navbar-item is-active" to="/portfolio">portfolio</NavLink>
-                  <NavLink className="navbar-item is-active" to="/contact">Contact</NavLink>
+                  <NavLink className="navbar-item is-active" to="/" onClick={() => {
+            setisActive(!isActive);
+          }}>Home</NavLink>
+                  <NavLink className="navbar-item is-active" to="/portfolio"
+                  onClick={() => {
+            setisActive(!isActive);
+          }}>portfolio</NavLink>
+                  <NavLink className="navbar-item is-active" to="/contact" onClick={() => {
+            setisActive(!isActive);
+          }}>Contact</NavLink>
                </div>
             </div>
          </div>
       </div>
-      </div>
+      
    );
 }
 
